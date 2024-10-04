0bf6e071cde0f8161a83119b84d7f8c276c60246
Sruthy24 <sruthysmnglm@gmail.com>
2024-10-04 16:09:16 +0530

    Add files via upload

---
+++ sruthy.py
@@ -0,0 +1,20 @@
+'''
+x,y,z="orange"," banana", "cherry"
+print(x)
+print(y)
+print(z)
+x="python"
+y="is"
+z="hello"
+print(x+y+z)
+username=input("name")
+print("name",username)
+'''
+'''
+a="orange"
+b="apple"
+c=a+b
+print(c)
+'''
+a="hellow world"
+print(a[0:5])
