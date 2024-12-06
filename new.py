da4bf9539c28c318ef2ff5d514a7bc34accbe294
Sruthy24 <sruthysmnglm@gmail.com>
2024-12-06 14:59:40 +0530

    Add files via upload

---
+++ new.py
@@ -0,0 +1,4 @@
+'''author-sruthy
+description-Program to define a module to find Fibonacci Numbers and import the module to another program.'''
+import fibonacci
+print(fibonacci.fibonacci_number(10))
