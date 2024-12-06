7c0b12bbd77c97b4daa1acffc6ad1541d0679159
Sruthy24 <sruthysmnglm@gmail.com>
2024-12-06 15:50:15 +0530

    Add files via upload

---
+++ gcd.py
@@ -0,0 +1,13 @@
+def gcd (num1,num2):
+    if num1 % num2  ==0:
+        return num2
+    else:
+        return gcd (num2,num1 % num2)
+num1=int(input("enter the first number:"))
+num2=int(input(" enter the second number: "))
+if num1>0 and num2>0:
+    print(f" the greatest common divisior o{num1} and {num2}is:",gcd (num1,num2))
+else:
+    print("enter positive number")
+
+
